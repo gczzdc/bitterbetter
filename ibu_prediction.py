e0d81555a7f9c5f11d7b03889893124a0a6c723d
Gabriel C. Drummond-Cole <gabriel.c.drummond.cole@gmail.com>
2019-10-10 16:19:59 -0700

    added static gradient

--- ibu_prediction.py
+++ ibu_prediction.py
@@ -9,7 +9,7 @@ def bag_of_words_paragraph(text):
 def best_predictor(text, abv=None, style=None):
 	return 35
 
-def generate_gradient(gradient_name, with_text=True, filename = 'static/gradient.png'):
+def generate_gradient(gradient_name='seismic', with_text=True, filename = 'static/gradient.png'):
 	gradient = np.linspace(0, 1, 256)
 	gradient = np.vstack((gradient, gradient))
 
@@ -19,7 +19,7 @@ def generate_gradient(gradient_name, with_text=True, filename = 'static/gradient
 
     # axes[0].set_title(cmap_category + ' colormaps', fontsize=14)
 
-	ax.imshow(gradient, aspect='auto', cmap=plt.get_cmap('coolwarm'))
+	ax.imshow(gradient, aspect='auto', cmap=plt.get_cmap(gradient_name))
 	if with_text:
 		ax.text(.2, 1.3, 'less bitter', 
 				va='center', 
