6ea48d6309bde98323468eacb50969846a84a8f3
Gabriel C. Drummond-Cole <gabriel.c.drummond.cole@gmail.com>
2019-10-10 13:54:52 -0700

    adding shell of flask app

---
+++ application.py
@@ -0,0 +1,15 @@
+import requests
+import pandas as pd
+import flask
+
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+@app.route('/index',methods=['GET','POST'])
+def index():
+	if flask.request.method in ['GET','POST']:
+		return (flask.render_template('index.html',{text:'Hello World'}))
+
+if __name__ == '__main__':
+	app.run(debug=True)
