ea1947a3b3e4a6f74763282744f372645dcff74d
Gabriel C. Drummond-Cole <gabriel.c.drummond.cole@gmail.com>
2019-10-10 16:30:07 -0700

    logic for bag of words paragraph

--- ibu_prediction.py
+++ ibu_prediction.py
@@ -1,9 +1,70 @@
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib import cm
+from matplotlib import colors
 
-def bag_of_words_paragraph(text):
-	return ''
+feature_coef_dic = {}
+
+#would be better if seismic were not hard-coded here and below but rather a global variable
+
+def colors_from_coef(coef, vmin = -40, vmax = 40, cmap = cm.seismic, cutoffs = [-6,6]):
+    norm = colors.Normalize(vmin=vmin, vmax=vmax, clip=True)
+    if coef < cutoffs[0] or coef > cutoffs[1]:
+        normalized_coef = norm(coef)
+        mapped_color = cmap(normalized_coef)
+        hex_color = colors.to_hex(mapped_color)
+        return hex_color
+    else:
+        return False
+
+
+def build_colors_plt(text, feature_coef_dic):
+    words = text.split()
+    ans_dic ={}    
+    for word in words:
+        if word in feature_coef_dic:
+            coef = feature_coef_dic[word]
+            hex_col = colors_from_coef(coef)
+            if hex_col:
+                ans_dic[word]={'hex_color':hex_col, 'coefficient':coef}
+    return ans_dic
+
+
+def bag_of_words_paragraph(text,features_coef_dic=feature_coef_dic):
+    working_dic = build_colors_plt(text,feature_coef_dic)
+    words = text.split()
+    para = []
+    style = []
+    counter = 1
+    for word in words:
+        if word not in working_dic:
+            para.append(word)
+            para.append(' ')
+        else:
+            para.append('<span ')
+            para.append('class="tooltip"')
+            para.append('style="background: ')
+            para.append(working_dic[word]['hex_color'])
+            para.append('"')
+            para.append('>')
+            para.append(word)
+            para.append('<span ')
+            para.append('class="tooltiptext" ')
+            para.append('style="color: ')
+            para.append(working_dic[word]['hex_color'])           
+            para.append('"')
+            para.append('>')
+            coef = working_dic[word]['coefficient']
+            if coef > 0:
+                para.append('+')    
+            para.append(str(int(coef)))
+            para.append('</span >')
+            para.append('</span >')
+            para.append(' ')
+            counter+=1
+    final= ''.join(para)
+    return final
 
 
 def best_predictor(text, abv=None, style=None):
